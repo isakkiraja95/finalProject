2541c315f207fae89d7282c49def964619246e13
isakkiraja95 <isakkiraja95@gmail.com>
2023-04-30 19:10:07 +0900

    Update player.py

--- final3NalinModified-v2/geoffrey_agent/player.py
+++ final3NalinModified-v2/geoffrey_agent/player.py
@@ -75,6 +75,7 @@ class Team:
            TODO: feel free to edit or delete any of the code below
         """
         self.team, self.num_players = team, num_players
+        print('New Game Geofrrey')
         return ['tux'] * num_players
 
     def act(self, player_state, opponent_state, soccer_state):
